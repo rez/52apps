cc3c6b7b326e3febca46450e301a92992022283c
J - <hotcross104@yahoo.com>
2019-03-28 09:38:27 -0400

    startup

--- .history/src/components/navigation_20190328091330.js
+++ .history/src/components/navigation_20190328091330.js
@@ -9,7 +9,7 @@ export default () => (
         <Link to="/">Home</Link>
       </li>
       <li className={styles.navigationItem}>
-        <Link to="/blog/">Blogg</Link>
+        <Link to="/blog/">Blog</Link>
       </li>
     </ul>
   </nav>
